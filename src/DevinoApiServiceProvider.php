4d9e2bbb2e6117c7f891947b5a7810205fee4909
trinzotoff <trin4ik@gmail.com>
2020-05-08 01:58:56 +0300

    0.1.2

--- src/DevinoApiServiceProvider.php
+++ src/DevinoApiServiceProvider.php
@@ -29,9 +29,9 @@ class DevinoApiServiceProvider extends ServiceProvider
     {
         //
         $this->publishes([
-            __DIR__.'config/devino.php' => config_path('devino.php'),
+            __DIR__.'/../config/devino.php' => config_path('devino.php'),
         ]);
-        $this->loadRoutesFrom(__DIR__.'/routes/routes.php');
-        $this->loadMigrationsFrom(__DIR__.'/database/2020_04_28_174807_create_devino_table.php');
+        $this->loadRoutesFrom(__DIR__.'/../routes/routes.php');
+        $this->loadMigrationsFrom(__DIR__.'/../database/2020_04_28_174807_create_devino_table.php');
     }
 }
